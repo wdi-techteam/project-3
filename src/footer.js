0ecd612eeeb16e6ca6d8d38e79c2803cda5f69f9
taha-sindi <taha.sindi@gmail.com>
2019-07-09 12:39:43 +0300

    Taha Commited

--- src/footer.js
+++ src/footer.js
@@ -2,10 +2,14 @@ import React from "react";
 const Footer = () => (
   
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ...
   <footer className="footer">
     <p className="copyright">COPYRIGHT 2019 {" "}</p>
     <p className="message">Created with &hearts;by
     <span>Tech Team</span></p> </footer> );
+<<<<<<< HEAD
 =======
   <footer>
     <p style="text-align: left;" class="copyright">COPYRIGHT 2019 {" "}</p>
@@ -13,5 +17,7 @@ const Footer = () => (
     <span style="color:red;">Tech Team</span></p>
   </footer> );
 >>>>>>> Doc Format
+=======
+>>>>>>> ...
 
 export default Footer;
