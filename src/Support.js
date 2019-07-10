74b260e2ed4b29350a424bb10d74cdfe50112951
MahaKarban <maha.ahmad.karban@gmail.com>
2019-07-10 15:42:32 +0300

    css

--- src/Support.js
+++ src/Support.js
@@ -1,18 +1,30 @@
 import React from "react";
+import github from './github.jpg'
+import mail from './email.jpg'
 
 const Support = () => ( 
 
-<div> {" "} <h1 className="tech"> Support </h1>
+<div>  <h1 className="about"> Support </h1>
     
-    <p className="team"> {" "}
+    <p className="we"> 
       We have a strong commitment to providing quality support, 
-      Please if you want to contact us: {" "} </p>
+      Please if you want to contact us: 
 
-    <ul className="us">
-      <li>Mr. Taha: taha.sindi@gmail.com </li>
-      <li>Ms. Wijdan: Wijdan.w.k@outlook.sa </li>
-      <li>Ms. Amany: amanyalattass@gmail.com </li>
-      <li>Ms. Maha: maha.ahmad.karban@gmail.com </li>
-    </ul>{" "} </div> );
+    <ul>
+      <li><span>Mr. Taha:<br/><a href="mailto:taha.sindi@gmail.com"> <img src={mail} alt="github logo" height='50px' /></a>
+<a href="https://github.com/taha-sindi"><img className="git" src={github} alt="github logo" height='50px'  /></a> </span> </li><br/>
+
+      <li><span>Ms. Wijdan:<br/><a href="mailto:Wijdan.w.k@outlook.sa"> <img className="mail" src={mail} alt="github logo" height='50px' /></a>
+<a href="https://github.com/wijdan1995"><img className="git" src={github} alt="github logo" height='50px' /></a>  </span></li><br/>
+      
+      
+      <li><span>Ms. Amany:<br/><a href="mailto:amanyalattass@gmail.com"> <img src={mail} alt="github logo" height='50px' /></a>
+<a href="https://github.com/AmanyAts"><img className="git" src={github} alt="github logo" height='50px' /></a>  </span></li><br/>
+      
+      <li><span>Ms. Maha:<br/> <a href="mailto:maha-ahmad@live.com"> <img src={mail} alt="github logo" height='50px' /></a>
+<a href="https://github.com/MahaKarban"><img className="git" src={github} alt="github logo" height='50px' /></a></span>     
+</li><br/>
+
+    </ul></p> </div> );
 
 export default Support;
