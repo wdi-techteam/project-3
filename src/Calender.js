ade9a7885c72a1d4e358026352b6db8f41818cf0
wijdan1995 <wijdan.w.k@outlook.sa>
2019-07-10 16:54:47 +0300

    change css

--- src/Calender.js
+++ src/Calender.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Calender() {
   let tempDate = new Date();
-  let date= tempDate.getFullYear() + '-' + (tempDate.getMonth()+1) + '-' + tempDate.getDate() 
-  
+  let date = tempDate.getFullYear() + '-' + (tempDate.getMonth() + 1) + '-' + tempDate.getDate()
+
   return (
 
-  <span className="Cal">{date}</span>
-  
+    <span className="Cal">{date}</span>
+
   );
 }
 export default Calender;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Calender from "./Calender"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 
 
 const Navbar = props => (
   <header>
     <nav>
-      <div className="Home"> <Link to="/">Home</Link>   <Link to="/about">About</Link> <Link to="/currencies">Currencies</Link> <Link to="/support">Support</Link> 
-      <Calender/></div>
+
+      <div className="Home"> <span className="title">Currancy Exchange</span><Link to="/">Home</Link>   <Link to="/about">About</Link> <Link to="/currencies">Currencies</Link> <Link to="/support">Support</Link>
+        <Calender /></div>
     </nav>
   </header>
 );
 
 
-export  default Navbar;
+export default Navbar;
