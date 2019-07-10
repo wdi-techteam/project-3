75eda89877126ceb4a09a7ba979f105dd170af58
wijdan1995 <wijdan.w.k@outlook.sa>
2019-07-10 17:26:01 +0300

    Updates

---
+++ precache-manifest.9b44a004e25170243aa4e98108cf6039.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30fa1a7af572c3989c578fdbc115935",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "b62061238d2bb7de6929",
+    "url": "/project-3/static/css/main.bca1da87.chunk.css"
+  },
+  {
+    "revision": "0450a6e5958a76d0d6db",
+    "url": "/project-3/static/js/2.842c4124.chunk.js"
+  },
+  {
+    "revision": "b62061238d2bb7de6929",
+    "url": "/project-3/static/js/main.cf08fc8b.chunk.js"
+  },
+  {
+    "revision": "7763db6ab8e7687ed2e1",
+    "url": "/project-3/static/js/runtime~main.6a465fa7.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/project-3/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "c4bd9291a65811a12a7d17f80abd592c",
+    "url": "/project-3/static/media/email.c4bd9291.png"
+  },
+  {
+    "revision": "2327295572ca2b219e2514041570687f",
+    "url": "/project-3/static/media/texture.23272955.jpg"
+  }
+]);
