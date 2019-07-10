c7d0909a9ba0de64a679473657860de637d9a2b2
MahaKarban <maha.ahmad.karban@gmail.com>
2019-07-10 16:24:36 +0300

    change about and support css

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ const About = () => (
     <h1 className="about"> About </h1>
     <p className="this-app">
       
-      <br/>
+      
       This app give you the power of our most up to date, reputable currency
       information and offer you secure, reliable, easy to use products and
       services dedicated to making your life easier.Its main function is 
--- src/Support.js
+++ src/Support.js
@@ -1,6 +1,6 @@
 import React from "react";
-import github from './github.jpg'
-import mail from './email.jpg'
+import github from './GitHub_Logo.png'
+import mail from './email.png'
 
 const Support = () => ( 
 
@@ -11,19 +11,19 @@ const Support = () => (
       Please if you want to contact us: 
 
     <ul>
-      <li><span>Mr. Taha:<br/><a href="mailto:taha.sindi@gmail.com"> <img src={mail} alt="github logo" height='50px' /></a>
-<a href="https://github.com/taha-sindi"><img className="git" src={github} alt="github logo" height='50px'  /></a> </span> </li><br/>
+      <li><span>Mr. Taha:<br/><a href="mailto:taha.sindi@gmail.com"> <img src={mail} alt="github logo" height='30px' /></a>
+<a href="https://github.com/taha-sindi"><img className="git" src={github} alt="github logo" height='30px'  /></a> </span> </li><br/>
 
-      <li><span>Ms. Wijdan:<br/><a href="mailto:Wijdan.w.k@outlook.sa"> <img className="mail" src={mail} alt="github logo" height='50px' /></a>
-<a href="https://github.com/wijdan1995"><img className="git" src={github} alt="github logo" height='50px' /></a>  </span></li><br/>
+      <li><span>Ms. Wijdan:<br/><a href="mailto:Wijdan.w.k@outlook.sa"> <img className="mail" src={mail} alt="github logo" height='30px' /></a>
+<a href="https://github.com/wijdan1995"><img className="git" src={github} alt="github logo" height='30px' /></a>  </span></li><br/>
       
       
-      <li><span>Ms. Amany:<br/><a href="mailto:amanyalattass@gmail.com"> <img src={mail} alt="github logo" height='50px' /></a>
-<a href="https://github.com/AmanyAts"><img className="git" src={github} alt="github logo" height='50px' /></a>  </span></li><br/>
+      <li><span>Ms. Amany:<br/><a href="mailto:amanyalattass@gmail.com"> <img src={mail} alt="github logo" height='30px' /></a>
+<a href="https://github.com/AmanyAts"><img className="git" src={github} alt="github logo" height='30px' /></a>  </span></li><br/>
       
-      <li><span>Ms. Maha:<br/> <a href="mailto:maha-ahmad@live.com"> <img src={mail} alt="github logo" height='50px' /></a>
-<a href="https://github.com/MahaKarban"><img className="git" src={github} alt="github logo" height='50px' /></a></span>     
-</li><br/>
+      <li><span>Ms. Maha:<br/> <a href="mailto:maha-ahmad@live.com"> <img src={mail} alt="github logo" height='30px' /></a>
+<a href="https://github.com/MahaKarban"><img className="git" src={github} alt="github logo" height='30px' /></a></span>     
+</li>
 
     </ul></p> </div> );
 
